a975bc8df54e6be5b7ba3f8e1bc0600a645548be
sdipietro <stevedipietro97@gmail.com>
2022-06-01 14:00:20 -0400

    display coins

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import Example from './scripts/example';
 
-// const abcd = new Example(main);
-// abcd.getData()
-//     .then(data => {
-//         // If the fetch was successful, here we can manipulate the data we received
-//         console.log(data);
-//     })
-//     .catch(error => {
-//         // If our fetch was unnsuccessful, here we can handle our error(s)
-//         console.error('There has been a problem with your fetch operation: ', error);
-//     }
-// );
-
 document.addEventListener("DOMContentLoaded", () => {
-  console.log('Hello World');
   const main = document.getElementById("main");
-  const abcd = new Example(main);
-  abcd.getData();
+  const test = new Example(main);
 });
--- src/scripts/example.js
+++ src/scripts/example.js
@@ -4,9 +4,9 @@ const CoinGeckoClient = new CoinGecko();
 class Example {
   constructor(el) {
     this.el = el;
-    this.el.innerHTML = "<h1>Show me some crypto</h1>"
+    this.el.innerHTML = "<h1>Show me some crypto!</h1>"
 
-    this.el.addEventListener("click", this.printData.bind(this))
+    this.el.addEventListener("click", this.getData.bind(this))
   }
 
   handleClick() {
@@ -18,50 +18,22 @@ class Example {
     var func = async() => {
         let data = await CoinGeckoClient.coins.all();
         return data;
-    };
-    func();
-
-    const ul = document.createElement("ul");
-    for (let rowIdx = 0; rowIdx < 3; rowIdx++) {
-      for (let colIdx = 0; colIdx < 3; colIdx++) {
-        const li = document.createElement("li");
-        // Setting data-* attributes with dataset will convert them automatically to a string
-        // However this will not be an easily parsible format for arrays
-        // We're using the JSON.stringify method to make it easier for us to use later
-        li.dataset.pos = JSON.stringify([rowIdx, colIdx])
-        // in order to get strings to an array, use JSON.parse
-        ul.append(li);
-      }
     }
-
-    this.el.append(ul);
-  }
-
-  printData() {
-    const ul = document.createElement("ul");
-    // const allCoins = this.getData();
-    // console.log(allCoins);
-    // debugger
-
-    // for (let rowIdx = 0; rowIdx < 3; rowIdx++) {
-    //   for (let colIdx = 0; colIdx < 3; colIdx++) {
-    //     const li = document.createElement("li");
-    //     // Setting data-* attributes with dataset will convert them automatically to a string
-    //     // However this will not be an easily parsible format for arrays
-    //     // We're using the JSON.stringify method to make it easier for us to use later
-    //     li.dataset.pos = JSON.stringify([rowIdx, colIdx])
-    //     // in order to get strings to an array, use JSON.parse
-    //     ul.append(li);
-    //   }
-    // }
-
-    // this.el.append(ul);
-
+    func().then(data => {
+        let dataObj = data.data;
+        const ul = document.createElement("ul");
+        for (let key in dataObj) {
+            let coinObj = dataObj[key];
+            let name = coinObj.name;
+            let symbol = coinObj.symbol;
+            const li = document.createElement("li");
+            li.innerText = `Name: ${name}         Symbol: ${symbol}`;
+            ul.append(li);
+        }
+        
+        this.el.append(ul);
+    })
   }
 };
-
-// let test = new Example();
-
-// test.getData();
 
 export default Example;
